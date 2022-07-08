799b509910020e2e8baddd1950cb7dc5aa537635
yabobay <yabobayyabobay@gmail.com>
2022-07-08 17:45:17 +0300

    fix the coloring code so u can have background fun

--- tiles/main.lua
+++ tiles/main.lua
@@ -5,7 +5,7 @@ function love.load()
 
   love.window.setMode(500, 500)
   loadPixels()
-  party = true
+  party = false
 
 end
 
@@ -13,16 +13,22 @@ function love.draw()
   for i, x in pairs(pixels) do
     pixelsize = columnsize / length(pixels[i])
     for j, y in pairs(x) do
-      if y ~= 0 then
-        love.graphics.rectangle (
-          "fill", pixelsize*(j-1), rowsize*(i-1), pixelsize, rowsize
-        )
+      if y ~= 0 then -- white pixel
         if party then
-          love.graphics.setColor (
-            math.random(), math.random(), math.random()
-          )
+          love.graphics.setColor (math.random(), math.random(), math.random())
+        else
+          love.graphics.setColor (1,1,1)
+        end
+      else -- black pixel
+        if party then
+          love.graphics.setColor (math.random() / 10 , math.random() / 10, math.random() / 10)
+        else
+          love.graphics.setColor(0,0,0)
         end
       end
+      love.graphics.rectangle (
+        "fill", pixelsize*(j-1), rowsize*(i-1), pixelsize, rowsize
+      )
     end
   end
 end
@@ -35,7 +41,6 @@ function love.keypressed(key, code, rep)
     -- switches on or off party mode
     if party then
       party = false
-      love.graphics.setColor(1,1,1) -- set color back to white
     else
       party = true
     end
