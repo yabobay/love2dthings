9eabf2d95ed4f2687a2f224f01f8e5bdfd8053c0
yabobay <yabobayyabobay@gmail.com>
2022-07-11 08:34:45 +0300

    add searching so youre not stuck pressing space
    will definitely have to clean this up later

--- bitmaps/main.lua
+++ bitmaps/main.lua
@@ -10,44 +10,76 @@ function love.load()
 end
 
 function love.draw()
-  -- "pixels" is the table with the pixel info in it
-  for i, x in pairs(pixels) do
-    pixelsize = columnsize / length(pixels[i])
-    for j, y in pairs(x) do
-      -- choose what color the pixel is
-      if party then
-        if y == 0 then
-          love.graphics.setColor (math.random() / 10 , math.random() / 10, math.random() / 10)
+  if not typing then
+    -- "pixels" is the table with the pixel info in it
+    for i, x in pairs(pixels) do
+      pixelsize = columnsize / length(pixels[i])
+      for j, y in pairs(x) do
+        -- choose what color the pixel is
+        if party then
+          if y == 0 then
+            love.graphics.setColor (math.random() / 10 , math.random() / 10, math.random() / 10)
+          else
+            love.graphics.setColor (math.random(), math.random(), math.random())
+          end
         else
-          love.graphics.setColor (math.random(), math.random(), math.random())
+          if y == 0 then
+            love.graphics.setColor (0,0,0)
+          elseif type(y) == "table" then
+            love.graphics.setColor (y[1], y[2], y[3])
+          else
+            love.graphics.setColor (1,1,1)
+          end
         end
-      else
-        if y == 0 then
-          love.graphics.setColor (0,0,0)
-        elseif type(y) == "table" then
-          love.graphics.setColor (y[1], y[2], y[3])
-        else
-          love.graphics.setColor (1,1,1)
-        end
+        -- actually draw the pixel
+        love.graphics.rectangle (
+          "fill", pixelsize*(j-1), rowsize*(i-1), pixelsize, rowsize
+        )
       end
-      -- actually draw the pixel
-      love.graphics.rectangle (
-        "fill", pixelsize*(j-1), rowsize*(i-1), pixelsize, rowsize
-      )
     end
+  else
+    love.graphics.setColor(1,1,1)
+    love.graphics.print("Load sprite:", 10, 10)
+    love.graphics.print(search, 10, 50)
   end
 end
 
 function love.keypressed(key, code, rep)
-  if key == "space" then
-    -- loads a sprite at random
-    loadPixels()
-  elseif key == "p" then
-    -- switches on or off party mode
-    if party then
-      party = false
-    else
-      party = true
+  if not typing then
+    if key == "space" then
+      -- loads a sprite at random
+      loadPixels()
+    elseif key == "p" then
+      -- switches on or off party mode
+      if party then
+        party = false
+      else
+        party = true
+      end
+    elseif key == "/" then
+      typing = true
+      search = ""
+    end
+  else
+    if key == "backspace" then
+      search = string.sub(search, 1, string.len(search) - 1)
+    elseif key == "return" then
+      typing = false
+      if gallery[search] then
+        loadPixels(search)
+      end
+    elseif key == "escape" then
+      typing = false
     end
   end
+end
+
+function love.textinput(t)
+  if typing then
+    search = search .. t
+  end
+end
+
+function love.update()
+  if search == "/" then search = "" end -- quick shitty fix
 end
