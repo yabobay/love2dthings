8ac409a8a1060e635adfba5312dd4b11f15356da
yabobay <yabobayyabobay@gmail.com>
2022-07-13 16:28:49 +0300

    fix png code lol

--- bitmaps/pngsprite.py
+++ bitmaps/pngsprite.py
@@ -1,43 +1,35 @@
 # not not not perfect!
 
 import png
+from sys import argv as args
 
-def list2table(bmap):
-  print("{")
-  for x in range(0, len(bmap)): # for every line
-    print("  {", end='')
-    for y in range(0, len(bmap[x])): # for every pixel
-      print("{", end='')
-      for z in range(0, len(bmap[x][y])): # for each color in the pixel
-        if z != len(bmap[x][y]) -1:
-          print(bmap[x][y][z], end=',')
-        else:
-          print(bmap[x][y][z], end='')
-      if y != len(bmap[x]) - 1:
-        print("}", end=',')
-      else:
-        print("}", end='')
-    if x != len(bmap) - 1:
-      print("},")
+def list2table(lis):
+  table = "{"
+  for i in range(len(lis)):
+    if type(lis[i]) == list:
+      table += list2table(lis[i])
     else:
-      print("}")
-  print("}")
+      table += str(lis[i])
+    if i != len(lis)-1:
+      table += ','
+  table += "}"
+  return table
 
-r = png.Reader(file=open("bee2.png", 'rb'))
+if len(args) > 1:
+  r = png.Reader(file=open(args[1], 'rb'))
+  readed = r.read()
 
-readed = r.read()
+  image = []
+  rows = list(readed[2])
 
-image = []
-rows = list(readed[2])
+  for x in range(len(rows)): # for every row
+    image.append([])
+    for i in range(0, len(rows[x]), 3): # for every pixel
+      image[x].append([])
+      for j in range(3):
+        col = rows[x][i+j]/255
+        if col == int(col):
+          col = int(col)
+        image[x][len(image[x])-1].append(col)
 
-for x in range(len(rows)): # for every row in the image
-  image.append([])
-  for i in range(0, len(rows[x]), 3): # for every pixel in the image
-    image[x].append([])
-    for j in range(3):
-      col = rows[x][i+j]/255
-      if col == int(col):
-        col = int(col)
-      image[x][len(image[x])-1].append(col)
-
-list2table(image)
+  print(list2table(image))
